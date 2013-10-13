8aa5903636e0942e6213fc1a5cbb5d94850575f5
Marin <mgs@jpisoftware.net>
2013-10-13 13:16:18 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 github-monitor
 ==============
 
-check github events in a shell script
+check github events in a shell script - pass a username to see recent events.
+
+Options:
+
+```sh
+  -c=0: cut after this length of output
+  -m=5: max number of events to display
+  -d=false: debug - read from file instead of request
+```
+
+I use it in conky like so: ${execi 3600 /opt/bin/gohub -c 30 hoffoo}
+
+Output:
+
+```sh
+burtbeckwith forked groovydev/twitter-bootstrap-grails-plugin
+guelfey commented guelfey/go.dbus
+apttap pushed hoffoo/jizzmatic
+aleksclark forked ebp/omniauth-box
+```
+
+Note that github allows 60 request per hour
---
+++ main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+var out io.Writer
+var limit int
+var skipEvents = []string{
+	"GistEvent",
+	"MemberEvent",
+}
+
+const (
+	BASE_URL     = "https://api.github.com/users/%s/received_events"
+	USERNAME int = 0
+)
+
+func main() {
+
+	var count = flag.Int("m", 5, "max number of events to display")
+	var debug = flag.Bool("d", false, "debug - read from file instead of request")
+	flag.IntVar(&limit, "c", 0, "cut after this length of output")
+
+	flag.Parse()
+	username := flag.Arg(USERNAME)
+
+	if username == "" {
+		fmt.Fprintf(os.Stderr, "Specify a username\n")
+		return
+	}
+
+	var r io.Reader
+	var resp *http.Response
+	var err error
+
+	if *debug == false {
+		resp, err = http.Get(fmt.Sprintf(BASE_URL, username))
+		r = resp.Body
+		defer resp.Body.Close()
+	} else {
+		r, err = os.Open("sample.json")
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	result := Receive(r)
+
+	out = os.Stdout
+
+	skipped := 0
+	for i, res := range result {
+		if res.summarize() {
+			skipped += 1
+		}
+
+		if i+1-skipped == *count {
+			break
+		}
+	}
+}
+
+type GithubJSON struct {
+	Actor struct {
+		Login, Url string
+	}
+	Repo struct {
+		Name, Url string
+	}
+	Payload struct {
+		Forkee struct {
+			Full_Name, Description string
+		}
+		Issue struct {
+			Url string
+		}
+		Target struct {
+			Login string
+		}
+		Action string
+	}
+	Type string
+}
+
+func (gj *GithubJSON) summarize() (skipped bool) {
+
+	switch gj.Type {
+	case "WatchEvent":
+		format("%s starred %s", gj.Actor.Login, gj.Repo.Name)
+	case "FollowEvent":
+		format("%s followed %s", gj.Actor.Login, gj.Payload.Target.Login)
+	case "IssuesEvent":
+		switch gj.Payload.Action {
+		case "opened":
+			format("%s issue %s", gj.Actor.Login, gj.Repo.Name)
+		}
+	case "IssueCommentEvent":
+		format("%s commented %s", gj.Actor.Login, gj.Repo.Name)
+	case "PushEvent":
+		format("%s pushed %s", gj.Actor.Login, gj.Repo.Name)
+	case "ForkEvent":
+		format("%s forked %s", gj.Actor.Login, gj.Repo.Name)
+	case "CreateEvent":
+		format("%s created %s", gj.Actor.Login, gj.Repo.Name)
+	case "PullRequestReviewCommentEvent":
+		format("%s commented %s", gj.Actor.Login, gj.Repo.Name)
+	default:
+		var hasEvent bool
+		for _, event := range skipEvents {
+			if event == gj.Type {
+				hasEvent = true
+				return true
+			}
+		}
+
+		if hasEvent == false {
+			format("-> %s %s %s", gj.Type, gj.Actor.Login, gj.Repo.Name)
+		}
+	}
+
+	return
+}
+
+func format(f string, args ...interface{}) {
+
+	f = fmt.Sprintf(f, args...)
+	if limit > 0 {
+		f = f[:limit]
+	}
+
+	out.Write([]byte(f + "\n"))
+}
+
+func Receive(r io.Reader) []GithubJSON {
+
+	data, err := ioutil.ReadAll(r)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var result []GithubJSON
+
+	err = json.Unmarshal(data, &result)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return result
+}
