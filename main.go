f08523458fad955839bff614c9b8f4eff1827a6e
Marin <mgs@jpisoftware.net>
2013-10-15 08:39:30 -0700

    no dupes, handle more events, shorter output

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 var out io.Writer
 var limit int
+var nodupes map[string]int
 var skipEvents = []string{
 	"GistEvent",
 	"MemberEvent",
@@ -30,6 +32,8 @@ func main() {
 
 	flag.Parse()
 	username := flag.Arg(USERNAME)
+
+	nodupes = make(map[string]int)
 
 	if username == "" {
 		fmt.Fprintf(os.Stderr, "Specify a username\n")
@@ -80,12 +84,15 @@ type GithubJSON struct {
 			Full_Name, Description string
 		}
 		Issue struct {
-			Url string
+			Url    string
+			Number int
 		}
 		Target struct {
 			Login string
 		}
-		Action string
+		Ref      string
+		Ref_Type string
+		Action   string
 	}
 	Type string
 }
@@ -98,23 +105,39 @@ func (gj *GithubJSON) summarize() (skipped bool) {
 	case "FollowEvent":
 		format("%s followed %s", gj.Actor.Login, gj.Payload.Target.Login)
 	case "IssuesEvent":
-		format("%s issue %s", gj.Actor.Login, gj.Repo.Name)
+		switch gj.Payload.Action {
+		case "created":
+			format("%s comment issue %d %s", gj.Actor.Login, gj.Payload.Issue.Number, gj.Repo.Name)
+		case "opened":
+			format("%s made issue %d %s", gj.Actor.Login, gj.Payload.Issue.Number, gj.Repo.Name)
+		case "closed":
+			format("%s closed issue %d %s", gj.Actor.Login, gj.Payload.Issue.Number, gj.Repo.Name)
+		default:
+			format("-> %s %s %s", gj.Type, gj.Actor.Login, gj.Repo.Name)
+		}
 	case "IssueCommentEvent":
 		format("%s commented %s", gj.Actor.Login, gj.Repo.Name)
 	case "PushEvent":
-		format("%s pushed %s", gj.Actor.Login, gj.Repo.Name)
+		format("%s pushed to %s", gj.Actor.Login, gj.Repo.Name)
 	case "ForkEvent":
 		format("%s forked %s", gj.Actor.Login, gj.Repo.Name)
 	case "CreateEvent":
-		format("%s created %s", gj.Actor.Login, gj.Repo.Name)
+		switch gj.Payload.Ref_Type {
+		case "tag":
+			format("%s tagged %s %s", gj.Actor.Login, gj.Payload.Ref, gj.Repo.Name)
+		case "repository":
+			format("%s created %s", gj.Actor.Login, gj.Repo.Name)
+		default:
+			format("-> %s %s %s", gj.Type, gj.Actor.Login, gj.Repo.Name)
+		}
 	case "PullRequestReviewCommentEvent":
 		format("%s commented %s", gj.Actor.Login, gj.Repo.Name)
 	case "PullRequestEvent":
 		switch gj.Payload.Action {
 		case "closed":
-			format("%s closed %s", gj.Actor.Login, gj.Repo.Name)
+			format("%s closed pull %s", gj.Actor.Login, gj.Repo.Name)
 		case "opened":
-			format("%s closed %s", gj.Actor.Login, gj.Repo.Name)
+			format("%s pull req %s", gj.Actor.Login, gj.Repo.Name)
 		default:
 			format("-> %s %s %s", gj.Type, gj.Actor.Login, gj.Repo.Name)
 		}
@@ -133,11 +156,18 @@ func (gj *GithubJSON) summarize() (skipped bool) {
 func format(f string, args ...interface{}) {
 
 	f = fmt.Sprintf(f, args...)
+	sameAuthorRepo := fmt.Sprintf("%s/", f[:strings.Index(f, " ")])
+	f = strings.Replace(f, sameAuthorRepo, "", 1)
+
 	if limit > 0 && limit < len(f) {
 		f = f[:limit]
 	}
 
-	out.Write([]byte(f + "\n"))
+	// squash duplicates
+	if _,dupe := nodupes[f]; dupe == false {
+		out.Write([]byte(f + "\n"))
+		nodupes[f] = 0
+	}
 }
 
 func Receive(r io.Reader) []GithubJSON {
